ce624b4af40a6caf117804f8a644775baac85749
Kristers Zarins <kristers.zarins@inbox.lv>
2013-12-08 21:00:18 +0000

    Add DB constants

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+//Database constants
+//Define them if they are not defined already
+defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost');
+defined('DB_USER')   ? null : define('DB_USER', 'kristers');
+defined('DB_PASS')   ? null : define('DB_PASS', 'chaikek');
+defined('DB_NAME')   ? null : define('DB_NAME', 'photo_gallery');
+?>
