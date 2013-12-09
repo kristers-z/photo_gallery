c7c14b214de32ccd5d59575c6a0527f183b84ab2
Kristers Zarins <kristers.zarins@inbox.lv>
2013-12-09 23:08:38 +0000

    Add user class

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
+require_once('../includes/database.php');
 
-echo 'this is working';
+
+//echo 'this is working<br>';
+//echo $database->escape_value("It's working?<br>");
+
+$sql =  "INSERT INTO users(id, username, password, first_name, last_name) ";
+$sql .= "VALUES(1, 'kskoglund', 'secretpwd', 'kevin', 'skoglund')";
+//$result = $database->query($sql);
+//
+
+$sql = "select * from users where id=1";
+$user = $database->fetch_array($database->query($sql));
+echo $user['username'];
 
 ?>
